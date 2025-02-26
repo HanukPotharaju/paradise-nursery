cbea74bcead1a3e01ef18de97ff4224ac88c7efb
Hanuk Potharaju <hanukpotharaju391@gmail.com>
2025-02-26 15:01:48 -0600

    Updated files for deployment and fixed missing changes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/paradise-nursery/" // Ensure this matches your GitHub repo name
+});
